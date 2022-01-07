ebf4d4535aa035d4e30a3bc98b3753fc1da86448
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-07 08:41:26 -0700

    Begin building #build_tree method

--- tree.rb
+++ tree.rb
@@ -2,5 +2,18 @@
 
 # class that holds logic for creating balanced binary search trees
 class Tree
-  # placeholder
+  attr_accessor :root
+
+  def initialize(data)
+    @root = build_tree(data)
+  end
+
+  def build_tree(array)
+    array.sort.uniq
+  end
 end
+
+# small test cases
+test_array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+my_tree = Tree.new(test_array)
+p my_tree.root
