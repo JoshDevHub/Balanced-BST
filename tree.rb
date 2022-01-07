b9d797f13b4c96d79f856327dcfa4e71f282b1e4
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-07 11:29:24 -0700

    Add #pretty_print method that prints formatted tree to terminal

--- tree.rb
+++ tree.rb
@@ -35,11 +35,18 @@ class Tree
     end
     curr_node
   end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
+  end
 end
 
 # small test cases
 test_array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 small_test = [1, 2, 3, 4]
-my_tree = Tree.new(small_test)
+my_tree = Tree.new(test_array)
 my_tree.insert(0)
 p my_tree.root
+my_tree.pretty_print
